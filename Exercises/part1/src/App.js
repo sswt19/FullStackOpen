497d52e3020069375503ca61d15c369123af36ce
sswt19 <saswatanoop@gmail.com>
2022-01-08 19:29:17 +0530

    Part 1a Exercises 1.1-1.2

---
+++ Exercises/part1/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Header = (props) => {
+  const course = props.course
+  return (<div>
+    <h1>{course}</h1>
+  </div>)
+}
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.totalExercises}</p>
+    </div>)
+}
+const Part = (props) => {
+  return (<div>
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  </div>)
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
+      <Total totalExercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
