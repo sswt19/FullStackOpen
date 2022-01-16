fb1a24d19d23219f840de1b97068967e019464e8
sswt19 <saswatanoop@gmail.com>
2022-01-16 22:47:38 +0530

    Part2c Exercise 2.11

--- Exercises/Part_2/part2_2_phonebook/src/App.js
+++ Exercises/Part_2/part2_2_phonebook/src/App.js
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+import axios from "axios";
 
-  const [personInfo, setPersonInfo] = useState({ name: "", number: "" })
-  const [searchName, setSearchName] = useState("")
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
+  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  // ]);
+
+  const hook = () => {
+    console.log("useEffect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
+  const [personInfo, setPersonInfo] = useState({ name: "", number: "" });
+  const [searchName, setSearchName] = useState("");
 
   const handleChangeInName = (event) => {
-    const copyPersonInfo = { ...personInfo, name: event.target.value }
-    setPersonInfo(copyPersonInfo)
+    const copyPersonInfo = { ...personInfo, name: event.target.value };
+    setPersonInfo(copyPersonInfo);
   };
   const handleChangeInNumber = (event) => {
-    const copyPersonInfo = { ...personInfo, number: event.target.value }
-    setPersonInfo(copyPersonInfo)
+    const copyPersonInfo = { ...personInfo, number: event.target.value };
+    setPersonInfo(copyPersonInfo);
   };
   const handleChangeInSearchName = (event) => {
     setSearchName(event.target.value);
@@ -42,13 +51,26 @@ const App = () => {
       }
     }
   };
-  const copyPersons = searchName.length > 0 ? persons.filter((person) => person.name.toLowerCase().includes(searchName.toLowerCase())) : persons
+  const copyPersons =
+    searchName.length > 0
+      ? persons.filter((person) =>
+        person.name.toLowerCase().includes(searchName.toLowerCase())
+      )
+      : persons;
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter searchName={searchName} handleChangeInSearchName={handleChangeInSearchName} />
+      <Filter
+        searchName={searchName}
+        handleChangeInSearchName={handleChangeInSearchName}
+      />
       <h3>Add a new</h3>
-      <PersonForm personInfo={personInfo} handleChangeInName={handleChangeInName} handleChangeInNumber={handleChangeInNumber} onSubmit={handleSubmit} />
+      <PersonForm
+        personInfo={personInfo}
+        handleChangeInName={handleChangeInName}
+        handleChangeInNumber={handleChangeInNumber}
+        onSubmit={handleSubmit}
+      />
       <h3>Numbers</h3>
       <Persons persons={copyPersons} />
     </div>
